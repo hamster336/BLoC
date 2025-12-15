db61e6a3aed31fe6b12e86b0597f4bb3e6bda82f
hamster336 <aashukalauni12@gmail.com>
2025-12-15 21:57:50 +0545

    Uusing cubit to update counter

---
+++ lib/counter_cubit.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit(super.initialState);
+
+  void increment() => emit(state + 1);
+
+  @override
+  void onChange(Change<int> change){
+    super.onChange(change);
+    print(change);
+  }
+}
